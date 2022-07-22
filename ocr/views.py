a227d3b798f52e623e5c4939867eb9b299f44fba
EngHassanAshraf <enghassanashraf@gmail.com>
2022-07-22 23:30:22 +0200

    ...

--- ocr/views.py
+++ ocr/views.py
@@ -10,15 +10,22 @@ class OCR(APIView):
     def get(self, request):
         return Response({"":""})
     def post(self, request):
+        print(1)
         analysisimage = request.FILES['image']
         try:
+            print(2)
             recvedimg = AnalysisImage.objects.create(imagetitle=str(analysisimage),analysisimage=analysisimage)
             print(f"image: {recvedimg}")
         except Exception as e:
             print(e)
+            
         ocrDriver(f"media/{recvedimg}")
+        print(3)
         ocrresults = get_result()
+        print(4)
         print(f" from ocr {ocrresults}")
+        print(5)
         translationDriver()
+        print(6)
         print(get_translated_ocr())
         return JsonResponse(get_translated_ocr(), safe=False)
