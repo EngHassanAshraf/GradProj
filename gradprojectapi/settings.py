f6988b55a718ef119ab178d2886c59f6720bf818
EngHassanAshraf <enghassanashraf@gmail.com>
2022-07-22 19:55:57 +0200

    settings updated

--- gradprojectapi/settings.py
+++ gradprojectapi/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -143,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'  #images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
